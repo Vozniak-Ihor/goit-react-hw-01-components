9ed2a839b4da80dc684ad25443cca4ce2294a229
Vozniak-Ihor <118538389+Vozniak-Ihor@users.noreply.github.com>
2023-05-21 16:43:56 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
+import { Profile } from "component/profile/profile";
+import { Statistics } from "component/statistics/statistics";
+import user from "./component/profile/user.json";
+import data from "./component/statistics/data.json";
+
 function App() {
   return (
-    <div className="App">
-      my
-    </div>
+    <>
+      <div className="App">
+        <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+      </div>
+      <Statistics 
+      title="Upload stats" 
+      statistics={data} 
+      />
+    </>
   );
 }
 
