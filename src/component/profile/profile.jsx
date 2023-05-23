c1346ec62323cf04fb6a55962211c845a67d907e
Vozniak-Ihor <118538389+Vozniak-Ihor@users.noreply.github.com>
2023-05-23 19:36:11 +0300

    finish task 1

--- src/component/profile/profile.jsx
+++ src/component/profile/profile.jsx
@@ -9,21 +9,21 @@ export const Profile = ({username,tag,location,avatar,stats:{followers,views,lik
         className={css.avatar}
       />
       <p className={css.name}>{username}</p>
-      <p className={css.tag}>{tag}</p>
+      <p className={css.tag}>@{tag}</p>
       <p className={css.location}>{location}</p>
     </div>
 
     <ul className={css.stats}>
       <li>
-        <span className={css.label}>Followers</span>
+        <span className={css.label}>Followers:</span>
         <span className={css.quantity}>{followers}</span>
       </li>
       <li>
-        <span className={css.label}>Views</span>
+        <span className={css.label}>Views:</span>
         <span className={css.quantity}>{views}</span>
       </li>
       <li>
-        <span className={css.label}>Likes</span>
+        <span className={css.label}>Likes:</span>
         <span className={css.quantity}>{likes}</span>
       </li>
     </ul>
